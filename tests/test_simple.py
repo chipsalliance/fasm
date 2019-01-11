8f8458dd90da36580e83d6d80def2690432a9d8a
Tim 'mithro' Ansell <me@mith.ro>
2019-01-10 23:07:18 -0800

    Fix formatting.
    Signed-off-by: Tim 'mithro' Ansell <me@mith.ro>

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -1,9 +1,11 @@
 import unittest
 import fasm
 
+
 def check_round_trip(test, result):
     s = fasm.fasm_tuple_to_string(result)
     test.assertEqual(list(fasm.parse_fasm_string(s)), result)
+
 
 class TestFasm(unittest.TestCase):
     def test_blank_file(self):
@@ -14,29 +16,36 @@ class TestFasm(unittest.TestCase):
 
     def test_comment_file(self):
         result = list(fasm.parse_fasm_filename('fasm_comment.fasm'))
-        self.assertEqual(result, [fasm.FasmLine(
-            set_feature=None,
-            annotations=None,
-            comment=' Only a comment.',
-            )])
+        self.assertEqual(
+            result, [
+                fasm.FasmLine(
+                    set_feature=None,
+                    annotations=None,
+                    comment=' Only a comment.',
+                )
+            ])
 
         check_round_trip(self, result)
 
     def test_one_line_feature(self):
         result = list(fasm.parse_fasm_filename('fasm_feature_only.fasm'))
-        self.assertEqual(result, [fasm.FasmLine(
-            set_feature=fasm.SetFasmFeature(
-                feature='EXAMPLE_FEATURE.X0.Y0.BLAH',
-                start=None,
-                end=None,
-                value=1,
-                value_format=None,
-                ),
-            annotations=None,
-            comment=None,
-            )])
+        self.assertEqual(
+            result, [
+                fasm.FasmLine(
+                    set_feature=fasm.SetFasmFeature(
+                        feature='EXAMPLE_FEATURE.X0.Y0.BLAH',
+                        start=None,
+                        end=None,
+                        value=1,
+                        value_format=None,
+                    ),
+                    annotations=None,
+                    comment=None,
+                )
+            ])
 
-        self.assertEqual(fasm.fasm_tuple_to_string(result), 'EXAMPLE_FEATURE.X0.Y0.BLAH\n')
+        self.assertEqual(
+            fasm.fasm_tuple_to_string(result), 'EXAMPLE_FEATURE.X0.Y0.BLAH\n')
         check_round_trip(self, result)
 
     def test_examples_file(self):
